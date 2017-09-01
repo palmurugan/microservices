aac0585cb95da0bbdec53e12d876be56e5554f59
pchandran <pchandran@ghx.com>
2017-09-02 00:11:09 +0530

    Initial Setup Added

---
+++ pands/src/main/java/com/macts/pands/App.java
@@ -0,0 +1,13 @@
+package com.macts.pands;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
