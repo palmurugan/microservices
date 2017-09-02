98bdb1d28dd775cb9f2622efa647397c8d0737e2
pchandran <pchandran@ghx.com>
2017-09-03 01:30:18 +0530

    Configuration Service Added

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 ptracker
 ========
 
-ptracker project repository
+pTracker : a microservice architechture project which will explain all microservice components.
+
+1) Eureka - Discovery Service
+2) Spring Cloud Configuration Server
+3) API Gateway
+4) Keycloak - Single Sign On
+5) ELK - Elastic, Logstash, Kibana
+6) Hystrix - Service to Service Call
+7) Ribbon - Load Balancer
+
+Note : Used all netflix components
 
 author : PalMurugan C
---
+++ configuration-service/src/main/java/com/macts/configuration/ConfigurationService.java
@@ -0,0 +1,21 @@
+package com.macts.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 
+ * @author PalMurugan C
+ *
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationService.class, args);
+	}
+
+}
