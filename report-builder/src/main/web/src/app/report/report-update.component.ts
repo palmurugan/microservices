aaa4fb2fe306ec05a99dcec2475f39bd041e10de
pchandran <pchandran@ghx.com>
2018-01-24 21:11:44 +0530

    Update Default View for Column and Filter

--- report-builder/src/main/web/src/app/report/report-update.component.ts
+++ report-builder/src/main/web/src/app/report/report-update.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Message } from 'primeng/components/common/api';
 import { QueryBuilderConfig } from '../../../lib/components/query-builder';
 import { Observable } from "rxjs/Observable";
 import { ReportService } from './report.service';
@@ -9,6 +10,7 @@ import { Resource } from '../domain/Resource';
     templateUrl: './report-update.component.html'
 })
 export class ReportUpdateComponent implements OnInit {
+    msgs: Message[] = [];
     reportId: number;
     isChecked: boolean = true;
     resources: Resource[];
@@ -33,7 +35,6 @@ export class ReportUpdateComponent implements OnInit {
             this.reportId = params['reportId'];
         });
         this.selected = [];
-        this.reportService.getAvailableColumn(1).then(availableColumn => this.available = availableColumn);
         this.reportService.getViewList().then(resources => this.resources = resources);
 
         this.firstFormGroup = this._formBuilder.group({
@@ -42,47 +43,70 @@ export class ReportUpdateComponent implements OnInit {
             description: '',
             public: ''
         });
-        this.reportDefinition = this.reportService.getReportDefinition(1);
+        this.reportDefinition = this.reportService.getReportDefinition(this.reportId);
         this.reportDefinition.subscribe(data => {
             this.firstFormGroup.patchValue({
-                public: data.public,
+                public: data.internal,
                 name: data.name,
                 description: data.description,
                 resourceView: data.resource.resourceId
             });
             this.selected = data.reportColumns;
+            this.loadFilter(data.resource.resourceId);
         });
-
-        this.query = this.reportService.getReportDefinition(1);
+        
+        this.query = this.reportService.getBasicQuery();
         this.query.subscribe(data => {
-            this.query = JSON.parse(data.reportFilter.filterQuery);
+            this.query = data;
         });
 
-        this.config = this.reportService.getFilterConfiguration(1);
+        this.config = this.reportService.getBasicFilterConfiguration();
+         this.config.subscribe(data => {
+             this.config = data;
+         });
+    }
+    
+    loadFilter(resourceId: number) {
+        this.reportService.getAvailableColumn(resourceId).then(availableColumn => this.available = availableColumn);
+        
+        this.query = this.reportService.getReportDefinition(this.reportId);
+        this.config = this.reportService.getFilterConfiguration(resourceId);
         this.config.subscribe(data => {
             this.config = data;
+             this.query.subscribe(data => {
+                this.query = JSON.parse(data.reportFilter.filterQuery);
+            });
         });
     }
 
-    saveReport() {
-        var data = {
-            "resource": {
-                "resourceId": this.resource
+    saveReportFilter() {
+        let i = 1;
+        const selectedColumns = [];
+        this.selected.forEach(column => {
+            selectedColumns.push({
+                'name': column.name,
+                'sequence': i,
+                'status': 'A'
+            });
+            i++;
+        });
+        const columnData = {
+            'resource': {
+                'resourceId': this.firstFormGroup.get('resourceView').value
             },
-            "reportColumns": [{
-                "name": "lastname",
-                "sequence": 1,
-                "status": "A"
-            }],
-            "reportFilter": {
-                "filterQuery": JSON.stringify(this.query)
+            'reportColumns': selectedColumns,
+            'reportFilter': {
+                'filterQuery': JSON.stringify(this.query)
             },
-            "name": this.name,
-            "description": this.description,
-            "internal": this.isChecked,
-            "status": "A"
-        }
-        console.log("Result Value :", data);
+            'name': this.firstFormGroup.get('name').value,
+            'description': this.firstFormGroup.get('description').value,
+            'internal': this.firstFormGroup.get('public').value,
+            'status': 'A'
+        };
+        this.reportService.updateReportDefinition(this.reportId, columnData).subscribe(result => {
+            // this.definitionId = result.definitionId;
+        });
+        this.msgs.push({ severity: 'success', summary: 'Success Message', detail: 'Report Updated' });
     }
 }
 
