5540c0a23c94f8560ad939c09c7dfeb388368fa8
Pal Murugan <murugan@healtheesystemsinc.com>
2018-06-12 23:16:17 +0530

    Swagger configuration

---
+++ party-management/src/main/java/com/genesis/party/config/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package com.genesis.party.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.genesis.party.web.rest"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("PTracker", "Party Management REST API's", "PTracker", "Terms of service",
+				new Contact("PalMurugan", "http://ptracker.in", "palmurugan.c@gmail.com"), "License of API",
+				"API license URL", Collections.emptyList());
+	}
+}
